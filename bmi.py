b3b2afc2d16cf461890bd1078dccfc94775f5748
Griselda Cortez <griseldacortez0218@gmail.com>
2021-03-15 17:02:14 -0400

    finished bmi

---
+++ bmi.py
@@ -0,0 +1,10 @@
+import sys # coding: utf-8
+
+print("BMI: Body Mass Index Calculator v.0.1 beta")
+userWeight = input("Enter your weight (in pounds): ")
+userHeight = input("Enter your weight (in inches): ")
+userHeightFloat + float(userHeight)
+
+myBMIpy = round((703 * float(userWeight)) / (userHeightFloat * userHeightFloat) ,2)
+
+print("Your body mass inex (BMI) is " + str(myBMIpy) + "%")
